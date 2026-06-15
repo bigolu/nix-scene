727b0a83bf1dcbd304cd01f8a673e5054a931541
Biggie <code@bigo.lu>
2026-06-15 09:46:44 -0400

    Remove unused dependency on coreutils

--- nix/outputs/package.nix
+++ nix/outputs/package.nix
@@ -2,12 +2,10 @@
   writeTextFile,
   lib,
   bash,
-  coreutils,
 }:
 let
-  inherit (lib) getExe fileContents toShellVar makeBinPath;
+  inherit (lib) getExe fileContents toShellVar;
   pname = "nix-scene";
-  dependencies = [ coreutils ];
 in
 # PERF: Since we don't cache this build we don't want to have many build
 # dependencies. For this reason, we don't use `resholve` since it depends on
@@ -22,12 +20,11 @@ writeTextFile {
     #!${getExe bash}
 
     ${toShellVar "NIX_SCENE_ENV" ../../src/env.nix}
-    export PATH=${makeBinPath dependencies}"''${PATH:+:$PATH}"
 
     ${fileContents ../../src/nix-scene.bash}
   '';
 
   passthru.devshellModule = {
-    devshell.packages = [ bash ] ++ dependencies;
+    devshell.packages = [ bash ];
   };
 }
